fbe6cd307824f0bf109b7801fc15a9e5edd4560b
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-07-07 22:36:31 +0300

    Implemented a swiper for the slides.

--- src/components/СonsultationForm.vue
+++ src/components/СonsultationForm.vue
@@ -1,80 +1,78 @@
 <template>
     <div class="consultation">
-        <div class="consultation__advertisement">
-            <h2 class="consultation__advertisement-title">
-                Уютные квартиры со скидкой 10%
-            </h2>
-            <a class="consultation__advertisement-link">
-                Узнать подробнее 
-                <font-awesome-icon icon="angle-right" />
-            </a>
-            <div class="consultation__advertisement-pagination">
-                <div>
-                    <font-awesome-icon icon="angle-left" />
-                    <span>1</span><span>/3</span>
-                    <font-awesome-icon icon="angle-right" />
+        <swiper
+            :modules="modules" 
+            :slides-per-view="1" 
+            :navigation="{
+                prevEl: '#swiper-prev',
+                nextEl: '#swiper-next'
+            }" 
+        >
+            <swiper-slide
+                v-for="item in 3"
+                :key="item"
+            >
+                <div class="consultation__advertisement">
+                    <h2 class="consultation__advertisement-title">
+                        Уютные квартиры со скидкой 10%
+                    </h2>
+                    <a class="consultation__advertisement-link">
+                        Узнать подробнее
+                        <font-awesome-icon icon="angle-right" />
+                    </a>
                 </div>
-            </div>
+            </swiper-slide>
+        </swiper>
+        <div class="consultation__advertisement-pagination">
+            <font-awesome-icon icon="angle-left" :id="'swiper-prev'" @click="changeSlideHandler('prev')" />
+            <span>{{ currentPage }}</span><span>/ {{ totalPages }}</span>
+            <font-awesome-icon icon="angle-right" :id="'swiper-next'" @click="changeSlideHandler('next')" />
         </div>
-
         <div class="consultation__form">
             <div class="consultation__form-top">
                 <h3 class="consultation__form-top-title">
                     Нужна консультация?
                 </h3>
                 <span class="consultation__form-top-description">
-                    Оставьте заявку. Мы свяжемся с вами в течение 
+                    Оставьте заявку. Мы свяжемся с вами в течение
                     <span class="consultation__mark">10 минут</span>
                 </span>
             </div>
 
             <div class="consultation__form-content">
                 <div class="consultation__form-content-item">
-                    <label class="consultation__form-content-title"> 
-                        <img src="../assets/img/icons/user-icon.svg" /> 
+                    <label class="consultation__form-content-title">
+                        <img src="../assets/img/icons/user-icon.svg" />
                         Ваше имя
                     </label>
-                    <input 
-                        placeholder="Ваше имя..." 
-                        class="consultation__form-content-field" 
-                    />
+                    <input placeholder="Ваше имя..." class="consultation__form-content-field" />
                 </div>
                 <div class="consultation__form-content-item">
                     <label class="consultation__form-content-title">
                         <img src="../assets/img/icons/email-icon.svg" />
                         Email
                     </label>
-                    <input 
-                        placeholder="Введите Email..." 
-                        class="consultation__form-content-field" 
-                    />
+                    <input placeholder="Введите Email..." class="consultation__form-content-field" />
                 </div>
                 <div class="consultation__form-content-item">
                     <label class="consultation__form-content-title">
                         <img src="../assets/img/icons/call-icon.svg" />
                         Номер телефона
                     </label>
-                    <input 
-                        placeholder="+7 (XXX) XXX-XX-XX" 
-                        class="consultation__form-content-field" 
-                    />
+                    <input placeholder="+7 (XXX) XXX-XX-XX" class="consultation__form-content-field" />
                 </div>
                 <div class="consultation__form-content-item">
                     <label class="consultation__form-content-title">
                         <img src="../assets/img/icons/question-icon.svg" />
                         Вопрос
                     </label>
-                    <input 
-                        placeholder="Оставьте комментарий..." 
-                        class="consultation__form-content-field" 
-                    
-                        />
+                    <input placeholder="Оставьте комментарий..." class="consultation__form-content-field" />
                 </div>
             </div>
 
             <div class="consultation__form-bottom">
                 <span class="consultation__form-bottom-confirmation">
-                    Нажимая на кнопку Отправить, я подтверждаю согласие на 
+                    Нажимая на кнопку Отправить, я подтверждаю согласие на
                     <span class="consultation__mark">
                         обработку персолнальных данных
 
@@ -90,33 +88,77 @@
 </template>
 
 <script>
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import VBasicBtn from './UI/v-basic-btn.vue';
 export default {
-    components:{
-        VBasicBtn
+    components: {
+        VBasicBtn,
+        Swiper,
+        SwiperSlide
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+        return {
+            onSwiper,
+            onSlideChange,
+            modules: [Navigation],
+        };
+    },
+    data(){
+        return{
+            currentPage: 1,
+            totalPages: 3
+        }
+    },
+    methods:{
+        changeSlideHandler(value){
+            switch (value) {
+                case 'prev': {
+                    if (this.currentPage > 1) this.currentPage--;
+                    break;
+                }
+                case 'next':{
+                    if (this.currentPage < this.totalPages) this.currentPage++;
+                    break;
+                }
+            }
+        }
     }
 }
-
 
 </script>
 
 <style scoped lang="scss">
-.consultation{
+.consultation {
     display: flex;
     justify-content: space-between;
     gap: 24px;
+    position: relative;
 
-    &__advertisement{
+    &__advertisement {
+        height: 100%;
         display: flex;
         flex-direction: column;
         gap: 32px;
         padding: 40px 32px;
         border-radius: 32px;
-        background: 
-            -70px bottom url('../assets/img/house-background.svg') no-repeat, 
+        background:
+            -70px bottom url('../assets/img/house-background.svg') no-repeat,
             #719B2D;
-
-        &-title{
+        box-sizing: border-box;
+            
+        &-title {
             font-size: 48px;
             font-weight: 700;
             line-height: 56px;
@@ -124,7 +166,7 @@ export default {
             margin: 0;
         }
 
-        &-link{
+        &-link {
             display: flex;
             align-items: center;
             gap: 14px;
@@ -133,18 +175,22 @@ export default {
             color: white;
         }
 
-        &-pagination{
-            flex-grow: 1;
+        &-pagination {
+            position: absolute;
+            bottom: 40px;
+            left: 350px;
+            z-index: 1;
             display: flex;
-            justify-content: flex-end;
-            align-items: flex-end;
+            justify-content: space-between;
+            align-items: center;
+            gap: 2px;
 
             color: white;
             opacity: .5;
         }
     }
 
-    &__form{
+    &__form {
         display: flex;
         flex-direction: column;
         gap: 70px;
@@ -153,12 +199,12 @@ export default {
         border-radius: 32px;
         background-color: #F6F6F6;
 
-        &-top{
+        &-top {
             display: flex;
             flex-direction: column;
             gap: 16px;
 
-            &-title{
+            &-title {
                 margin: 0;
                 font-size: 32px;
                 font-weight: 500;
@@ -166,26 +212,26 @@ export default {
                 color: #3C3C3B;
             }
 
-            &-description{
+            &-description {
                 font-size: 16px;
                 color: #3C3C3B;
             }
         }
 
-        &-content{
+        &-content {
             width: 800px;
             display: flex;
             justify-content: space-between;
             flex-wrap: wrap;
             gap: 32px 22px;
 
-            &-item{
+            &-item {
                 display: flex;
                 flex-direction: column;
                 gap: 18px;
             }
 
-            &-title{
+            &-title {
                 display: flex;
                 align-items: center;
                 gap: 8px;
@@ -197,7 +243,7 @@ export default {
                 color: #3C3C3B;
             }
 
-            &-field{
+            &-field {
                 display: flex;
                 flex-direction: column;
                 width: 324px;
@@ -207,18 +253,18 @@ export default {
             }
         }
 
-        &-bottom{
+        &-bottom {
             display: flex;
             gap: 50px;
             justify-content: space-between;
 
-            &-confirmation{
+            &-confirmation {
                 width: 372px;
                 font-size: 14px;
                 color: #9A9A9A;
             }
 
-            &-btn{
+            &-btn {
                 flex: 1;
                 padding: 16px, 24px;
 
@@ -234,7 +280,7 @@ export default {
         }
     }
 
-    &__mark{
+    &__mark {
         color: #719B2D
     }
 }
